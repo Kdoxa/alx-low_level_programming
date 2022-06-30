8dca8e31ffedf4fcb2baa31f1ba2fc2201555d50
Kdoxa <gloryola3@gmail.com>
2022-06-30 16:37:01 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-1 #include <stdio.h>
-2
-3 int main(void)
-	4 {
-		5 puts("/"Programming is like building a multilingual puzzle");
-		6 return(0);
-		7 };
+#include <stdio.h>
+/
+ * main - Entry point
+ *  * Return: Always 0 (sucess)
+ *   */
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
