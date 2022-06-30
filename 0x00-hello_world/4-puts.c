ff07810abd3cbaf6095b15795ca929972c018c4d
Kdoxa <gloryola3@gmail.com>
2022-06-30 16:27:21 +0000

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-#!/bin/bash
+#include <studio.h>
 
 /**
  * main -Entry point
- * Description: prints "programming is like building a multilingua puzzle"
- *
+ * 
+ * Description: prints "Programming is like building a multilingual puzzle
+ * 
  * Return: Always 0 (Success)
  * */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("/"Programming is like building a multilingual puzzle");
 	return (0);
-}
