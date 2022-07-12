ceb0d8e337f9007d393b4f1a1327bdef8f1c0bf4
Kdoxa <gloryola3@gmail.com>
2022-07-12 10:52:55 +0000

    Print Reverse;

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev -> prints a string reversely
+ * @s: string
+ */
+void print_rev(char *s);
+{
+	int asdf + 0;
+
+	while (asdf >= 0)
+	{
+		if (s[asdf] == '\0')
+			break;
+		asdf++;
+	}
+
+	for (asdf--; asdf >= 0; asdf--)
+		_putchar(s[asddf]);
+	_putchar('\n');
+}
