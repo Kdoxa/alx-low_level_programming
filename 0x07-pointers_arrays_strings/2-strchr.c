99641f4662cfeb9544d4e090e5cf4af906d14009
Kdoxa <gloryola3@gmail.com>
2022-07-18 11:23:11 +0000

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr -> it locates a chracter in a string
+ * @s: given string
+ * @c: char parameter
+ * Return: a string
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL):
+		}
+	}
+}
