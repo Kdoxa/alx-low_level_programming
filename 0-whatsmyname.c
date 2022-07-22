4abf743a19452b116329d8e1ea4b7afe08fe47a1
Kdoxa <gloryola3@gmail.com>
2022-07-22 17:12:32 +0000

    main.h

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
  * main -> this is a function to print its name
- * @argc: argc parameter
+ * @argc: argument parameter
  * @argv: an array of a command listed
- * Return: 0 for success
- */
-int main(int argc __attribute__((unused)), char *argv[])
+ * Return: 0 
+ **/
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
