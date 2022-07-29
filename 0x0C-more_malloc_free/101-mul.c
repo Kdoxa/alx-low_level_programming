0dcbecfec8f90b9c16adce4c1528ca0b0e847b9d
Kdoxa <gloryola3@gmail.com>
2022-07-29 19:02:12 +0000

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,6 +9,7 @@
 int is_digit(char *s)
 {
 	int i = 0;
+
 	while (s[i])
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -25,6 +26,7 @@ int is_digit(char *s)
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
@@ -48,7 +50,9 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
+
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
@@ -67,7 +71,7 @@ int main(int argc, char *argv[])
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 *digit2);
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
