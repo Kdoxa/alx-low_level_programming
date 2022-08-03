921f066f7c428de092dddbd5c0181e8148c8fcb1
Kdoxa <gloryola3@gmail.com>
2022-08-03 15:29:55 +0000

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_indes - it searches for an integer
+ * @array: input integer array
+ * @size: array size
+ * @cmp: pointer to the functn 
+ *  * 
+ *  Return: index of the first element for which the cmp function does not return 0
+ *  If no element matches, return -1
+ *  If size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
+		for (a = 0; a < size; a++)
+			if (cmp(array[a]))
+				return (a);
+	}
+	return (-1);
+}
